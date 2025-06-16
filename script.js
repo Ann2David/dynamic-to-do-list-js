13fd9ed42e3d8fbd4f8a380391c707656ccfcaa6
Ann David <annapauladav@gmail.com>
2025-06-16 13:44:25 -0700

    Modify script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     
-    const addButton = document.getElementById('add-task');
-    const taskInput = document.getElementById('task-input');
-    const taskList = document.getElementById('task-list');
-
+   const addButton = document.getElementById('add-task');     
+   const taskInput = document.getElementById('task-input');     
+   const taskList = document.getElementById('task-list');   
     
     function addTask() {
         
